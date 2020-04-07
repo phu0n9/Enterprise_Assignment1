29f18f0a8a9cf29a0f44e08bc7fa0b1e8c952b23
phu0n9 <pahn1303@gmail.com>
2020-04-08 00:38:39 +0700

    service section left

--- cmd.js
+++ cmd.js
@@ -7,6 +7,18 @@ function clickableNavBar(n){
     navBarItem[n-1].className += " navActive";
 }
 
+var n = 0;
+function toggleIconClickCount(){
+    var navigationBar = document.querySelector('nav');
+    n++;
+    if(n % 2 != 0){
+        navigationBar.style.display = "block";
+    }
+    else{
+        navigationBar.style.display = "none";
+    }
+}
+
 //Slides
 var slideIndex = 0;
 var slides = document.getElementsByClassName("slides");
@@ -47,7 +59,6 @@ function carousel() {
 }
 
 
-
 //service section movement
 var sectionBox = document.getElementsByClassName("service-section-box");
 var imageBox = document.getElementsByClassName("service-image-box");
@@ -59,31 +70,91 @@ function mouseOverServiceSection(n){
     imageBox[n-1].className += " upAndDown";
 }
 
+// Initialize and add the map
+// function initMap() {
+//     // The location of RMIT
+//     var rmit = {lat: 10.72986, lng: 106.69408};
+//     // The map, centered at RMIT
+//     var map = new google.maps.Map(document.getElementById('mapholder'), {zoom: 4, center: rmit});
+//     // The marker, positioned at RMIT
+//     var marker = new google.maps.Marker({position: rmit, map: map});
+// }
+
 
 //Form validation
-//FIXME:sticky navigation bar + form validation +smooth scroll
-var flag = 0;
+var checkPoint = 0;
+function validateName(stringName){
+    var lettersOnly = /^[A-Za-z]+$/;
+    if(document.getElementById(stringName).value.match(lettersOnly)){
+        document.getElementById(stringName).setCustomValidity("");
+        checkPoint = 1;
+    }
+    else{
+        document.getElementById(stringName).setCustomValidity("Please enter your name correctly.");
+        checkPoint = 0;
+    }
+}
 
- function validateEmail(){
- var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-     if(document.getElementById("email").value.match(mailformat)) {
-         document.getElementById("email").setCustomValidity('');
-         flag = 1;
-     }
-     else{
-         document.getElementById("email").setCustomValidity("You have entered an invalid email address!");
-         flag = 0;
-     }
+function validateGender(stringGender){
+    var genderOption = document.getElementsByTagName("option");
+    for (var i = 0; i < genderOption.length; i++) {
+        if(document.getElementById(stringGender).value == genderOption[0].value){
+            document.getElementById("gender").setCustomValidity("Please choose your gender");
+            document.getElementById("gender").style.border = "1px solid red";
+        }
+        else{
+            document.getElementById("gender").setCustomValidity("");
+            document.getElementById("gender").style.border = "1px solid #ccc";
+        }
+    }
+}
+
+function validateEmail(){
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(document.getElementById("email").value.match(mailformat)) {
+    document.getElementById("email").setCustomValidity('');
+    checkPoint = 1;
+    }
+    else{
+    document.getElementById("email").setCustomValidity("You have entered an invalid email address!");
+    checkPoint = 0;
+    }
  }
 
- function phonenumber(){
-     var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-     if((document.getElementById('phone').value.match(phoneno))){
-         document.getElementById("phone").setCustomValidity('');
-         flag = 1;
-     }
-     else{
-         document.getElementById("phone").setCustomValidity("Your phone input is incorrect!");
-         flag = 0;
-     }   
- }
+function validatePhoneNumber(){
+    var phoneno = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
+    if((document.getElementById('phone').value.match(phoneno))){
+        document.getElementById("phone").setCustomValidity('');
+        checkPoint = 1;
+    }
+    else{
+        document.getElementById("phone").setCustomValidity("Your phone input is incorrect!");
+        checkPoint = 0;
+    }   
+}
+
+function validateTextArea(){
+    var text = document.getElementById("comment-box");
+    var textLeft = 300 - text.value.length;
+    document.getElementById("counting-text").innerHTML = "only "+ textLeft +" characters left";
+    if(text.value == ""){
+        text.setCustomValidity("Your question(s) should not be empty.");
+        console.log("false");
+        checkPoint = 0;
+    }
+    else{
+        text.setCustomValidity('');
+        console.log("true");
+        checkPoint = 1;
+    }
+}
+
+function submitQuestion(){
+    if(checkPoint){
+        alert("Thank you for your question. We will provide the answer(s) through your email soon.");
+        window.location.href = "homepage.html";
+    }
+    else{
+        alert("Your input is wrong. Check again.");
+    }
+}
